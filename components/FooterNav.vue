bc5143da6bdba74e8ad46615c09fe0bba8199905
“shin-701” <shin.nakayama.1028@gmail.com>
2026-01-12 15:26:07 +0900

    不要なコンポーネントは削除

--- components/FooterNav.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <footer class="border-t border-gray-300">
-    <div class="container py-4 sm:py-6">
-      <p class="text-xs sm:text-sm md:text-base text-center">© {{ new Date().getFullYear() }} shin-701. All rights reserved.</p>
-    </div>
-  </footer>
-</template>
